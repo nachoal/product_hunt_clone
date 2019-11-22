fe50c6ee44a1add0a278cdd18d22af06b57b6a94
nachoal <nacho.allibre@gmail.com>
2019-11-21 19:50:53 -0600

    Prepare travis

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'full_name returns the capitalized first name and last name' do
+    user = User.new(first_name: 'ignacio', last_name: 'alonso')
+    assert_equal 'Ignacio Alonso', user.full_name
+  end
 end
